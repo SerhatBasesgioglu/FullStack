2004b0883a6b80ea9b072b72b738efa0d91b936d
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-08-13 19:34:10 +0300

    scrap canvas for now, add input for message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import Canvas from "./components/Canvas";
 import Message from "./components/Message";
 import TestButton from "@/components/TestButton";
 
 function App() {
   return (
     <>
-      <Canvas />
       <Message />
       <TestButton />
     </>
--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,20 +1,30 @@
 import { subscribeChat, publishChat } from "@/util/websocket";
+import { useState } from "react";
 
 const Message = () => {
+  const [message, setMessage] = useState("");
   const handleSubscribe = () => {
     subscribeChat();
   };
 
-  const handlePublish = (message) => {
+  const handlePublish = () => {
     publishChat(message);
   };
+
   return (
     <>
       <div>
         <button onClick={handleSubscribe}>Subscribe</button>
-        <button onClick={() => handlePublish("This is the published message")}>
-          Publish
-        </button>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+        <button onClick={handlePublish}>Send</button>
       </div>
       <p>This is the message field</p>
     </>
