028f150b845b2c50fc0c1878c9f24ef9c79c5980
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-08-13 22:02:54 +0300

    changed project from react with js to next with ts

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Image from "next/image";
+import Message from "@/components/Message";
+
+export default function Home() {
+  return (
+    <main>
+      <Message />
+    </main>
+  );
+}
