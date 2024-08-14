647e36368d04bbfef47ddfd31594bcb223fd3a76
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-08-14 08:50:21 +0300

    add player entity

--- backend/src/main/java/com/aydakar/backend/Entity/Player.java
+++ backend/src/main/java/com/aydakar/backend/Entity/Player.java
@@ -1,6 +1,9 @@
 package com.aydakar.backend.Entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -8,12 +11,14 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
-public class Message {
+public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private String content;
+    private String name;
+
+    private String whatever;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
